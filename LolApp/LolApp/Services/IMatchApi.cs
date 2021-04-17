bd47b7b482bcfad66385a0a0771ea06a5e8c379e
JohnEspinal <johnmespinalh@gmail.com>
2021-04-16 22:17:57 -0400

    Latest changes

---
+++ LolApp/LolApp/Services/IMatchApi.cs
@@ -0,0 +1,18 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolApp.Services
+{
+    public interface IMatchApi
+    {
+        [Get("by-puuid/{puuid}/ids")]
+        Task<HttpResponseMessage> GetMatches(string puuid, string key);
+
+        [Get("{matchId}")]
+        Task<HttpResponseMessage> GetMatchById(string matchId, string key);
+    }
+}
---
+++ LolApp/LolApp/Services/MatchApiService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolApp.Services
+{
+    public class MatchApiService : IMatchApi
+    {
+        ILolIconsApiService LolIconsApiService;
+        public MatchApiService(ILolIconsApiService lolIconsApiService)
+        {
+            LolIconsApiService = lolIconsApiService;
+        }
+
+        public Task<HttpResponseMessage> GetMatchById(string matchId, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> GetMatches(string puuid, string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
