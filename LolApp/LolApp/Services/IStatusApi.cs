02ad99030ace673a0bbb41ee0f98817f4e8dad71
JohnEspinal <johnmespinalh@gmail.com>
2021-04-16 21:58:20 -0400

    Revert "Changes"
    This reverts commit 2ea879af3f01352156e2dd0aa82bb2a905be37f2.

---
+++ LolApp/LolApp/Services/IStatusApi.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolApp.Services
+{
+    public interface IStatusApi
+    {
+        [Get("/RANKED_SOLO_5x5?api_key={key}")]
+        Task<HttpResponseMessage> GetStatusAsync(string key);   
+    }
+}
---
+++ LolApp/LolApp/ViewModels/GrandMasterViewModel.cs
@@ -0,0 +1,47 @@
+﻿using LolApp.Models;
+using LolApp.Services;
+using Prism.Services;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Entry = LolApp.Models.Entry;
+
+namespace LolApp.ViewModels
+{
+    public class GrandMasterViewModel : BaseViewModel
+    {
+        public string Title { get; set; } = "X Page";
+        public Entry Entries { get; set; }
+        public Status Status { get; set; }
+        IStatusApiService StatusApiService { get; }
+
+
+        public GrandMasterViewModel(IStatusApiService statusApiService, IPageDialogService alertService) : base(alertService)
+        {
+            StatusApiService = statusApiService;
+            Status = new Status();
+            GetStatus();
+        }
+
+        private async void GetStatus()
+        {
+            if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+            {
+                Status = await StatusApiService.GetStatusAsync();
+                Entries = Status.Entries[0];
+
+            }
+            else
+            {
+                // Alert
+                await AlertService.DisplayAlertAsync("No connection :( ", "please try again when your are conected.", "Ok", null);
+            }
+
+        }
+    }
+}
